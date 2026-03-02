6708ac15ae3290e030675da6ed3d68ecd4da3052
evanbones <bownessevan@gmail.com>
2026-03-02 12:54:37 -0800

    fix: cleanup app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from './components/Button'
 import { Header } from './components/Header'
 import JobPosting from "./components/JobPosting";
 
