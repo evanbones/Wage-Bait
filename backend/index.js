bbc263e414c9984aefd3195c595144be4f3b878d
evanbones <bownessevan@gmail.com>
2026-03-09 12:25:56 -0700

    feat: backend init

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/submit', (req, res) => {
+    const formData = req.body;
+
+    console.log("Hell yeah, got some stuff from React: ", formData);
+
+    res.status(200).json({
+        message: "Got yer data",
+        recievedData: formData
+    });
+});
+
+app.get('/api/get', (req, res) => {
+    const formData = req.body;
+
+    console.log("Hell yeah, got some stuff from React: ", formData);
+
+    res.status(200).json({
+        message: "Got yer data",
+        recievedData: formData
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Express running on http://localhost:${PORT}`);
+});
