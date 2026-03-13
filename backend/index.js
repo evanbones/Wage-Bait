d2412893ccf549a9172b71fb793d1773f63b8ad7
evanbones <bownessevan@gmail.com>
2026-03-12 18:28:17 -0700

    fix: database

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import { searchJobs } from './controllers/searchController.js';
-import { registerUser } from './controllers/userController.js';
-import Database from './db/connection.js';
+import express from "express";
+import cors from "cors";
+import { searchJobs } from "./controllers/searchController.js";
+import { registerUser } from "./controllers/userController.js";
+import Database from "./db/connection.js";
 
 const app = express();
 const PORT = 8000;
 
-const db = new Database(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const db = new Database(process.env.MONGODB_URI);
 
 db.connect().catch((err) =>
-  console.error("Error connecting to database:", err)
+  console.error("Error connecting to database:", err),
 );
 
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.post('/api/register', registerUser);
-app.get('/api/search', searchJobs);
-
-app.listen(PORT, () => {
-    console.log(`Express running on http://localhost:${PORT}`);
-});
+app.post("/api/register", registerUser);
+app.get("/api/search", searchJobs);
 
 process.on("SIGINT", async () => {
   try {
     await db.disconnect();
-    console.log("Disconnected from database.");
-    process.exit(0);
+
+    app.listen(PORT, () => {
+      console.log(`Express running on http://localhost:${PORT}`);
+    });
   } catch (err) {
     console.error(err);
     process.exit(1);
