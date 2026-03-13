fc9b6010237d7a5f2a4e73063e0bf3a08fc7d860
evanbones <bownessevan@gmail.com>
2026-03-12 17:27:36 -0700

    feat: add mongo scaffolding

---
+++ backend/db/connection.js
@@ -0,0 +1,22 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  await client.connect();
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged. Wahoo! You're connected to MongoDB!!");
+} catch (err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
+
+export default db;
