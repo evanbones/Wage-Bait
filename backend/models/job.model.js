ca01084919757568a121b3eaeeefd29c9c99b3b7
Patrick Rinn <144386801+PatrickRinn@users.noreply.github.com>
2026-03-17 11:15:41 -0700

    update data models and packages

---
+++ backend/models/job.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const bidSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    minimumSalary: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const jobSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        salary: {
+            type: Number,
+            required: true,
+        },
+        company: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        location: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        remote: {
+            type: Boolean,
+            default: false,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+            trim: true,
+            default: "Other"
+        },
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        bids: [bidSchema]
+    }
+);
+
+export default mongoose.model("Job", jobSchema);
