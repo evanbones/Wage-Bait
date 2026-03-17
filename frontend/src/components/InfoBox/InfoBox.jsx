613691de64400c1013b5b77fa09191713c9c3650
Cohen Kucher <cohenkucher@Cohens-MacBook-Air.local>
2026-03-17 13:23:26 -0700

    fixed some css stuff

--- frontend/src/components/InfoBox/InfoBox.jsx
+++ frontend/src/components/InfoBox/InfoBox.jsx
@@ -5,7 +5,6 @@ export const InfoBox = ({ label, type = "text", value, onChange }) => {
   return (
     <div className="info-box">
       <label className="info-label">{label}</label>
-      <br />
       <input
         type={type}
         className="field-input"
